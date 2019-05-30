4d00a37222ddf5615386db4c3775d47dcff9a8c4
Ben <carey.benjamin42@gmail.com>
2019-05-30 18:33:13 -0500

    initial commit

---
+++ src/test/java/com/btcarey/example/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btcarey.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GradleDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		Assert.assertTrue(true);
+	}
+
+}
